3e40008166e15868556171e19c937306d6ed91c8
MohammadReza Davoodabadi /Davidabadi <51988179+mrdair@users.noreply.github.com>
2024-04-02 01:02:18 +0330

    update-dbt-file

--- dbt/models/tmdb_popularity_chart.sql
+++ dbt/models/tmdb_popularity_chart.sql
@@ -3,7 +3,7 @@ WITH popularity_agg AS (
     TO_CHAR(TO_DATE(release_date, 'YYYY-MM-DD'), 'MMMM') AS month_name,
     ROUND(AVG(popularity), 2) AS popularity
 FROM 
-    tmdb_dataset.DEV.TMDB_MOVIES_MODELS
+    {{ ref("tmdb_movies_models") }}
 WHERE 
     release_date != ''
 GROUP BY 
